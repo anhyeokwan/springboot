f5a891505a70126ab9313d907452a01391df4468
anhyeongkwan <bombom841@naver.com>
2023-03-01 05:01:32 +0900

    - 커밋

--- src/main/java/org/zerock/b01/SampleController.java
+++ src/main/java/org/zerock/b01/SampleController.java
@@ -1,4 +1,90 @@
 package org.zerock.b01;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Log4j2
+@Controller
 public class SampleController {
+    @GetMapping("/hello")
+    public void hello(Model model){
+        log.info("hello.........");
+
+        model.addAttribute("msg", "Hello World");
+
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+
+        model.addAttribute("list", list);
+    }
+
+    class SampleDTO{
+        private String p1, p2, p3;
+
+        public String getP1() {
+            return p1;
+        }
+
+        public void setP1(String p1) {
+            this.p1 = p1;
+        }
+
+        public String getP2() {
+            return p2;
+        }
+
+        public void setP2(String p2) {
+            this.p2 = p2;
+        }
+
+        public String getP3() {
+            return p3;
+        }
+
+        public void setP3(String p3) {
+            this.p3 = p3;
+        }
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model){
+        log.info("ex/ex2..........");
+
+        List<String> list = IntStream.range(1, 10)
+                .mapToObj(i -> "Data" + i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value --- p1";
+        sampleDTO.p2 = "Value --- p2";
+        sampleDTO.p3 = "Value --- p3";
+
+        model.addAttribute("dto", sampleDTO);
+
+
+    }
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model){
+        model.addAttribute("arr", new String[]{"AAA", "BBB", "CCC"});
+    }
 }
---
+++ src/main/java/org/zerock/b01/SampleJSONController.java
@@ -0,0 +1,48 @@
+package org.zerock.b01;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@Log4j2
+public class SampleJSONController {
+
+    @GetMapping("/helloArr")
+    public String[] helloArr() {
+
+        log.info("helloArr...........");
+
+        return new String[]{"AAA", "BBB", "CCC"};
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
