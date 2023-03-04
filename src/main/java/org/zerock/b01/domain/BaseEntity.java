ee91b4d8297713262bb175ec23a605e07c9e4aad
anhyeongkwan <bombom841@naver.com>
2023-03-05 03:15:29 +0900

    - 자바 웹개발 437p 완료

--- src/main/java/org/zerock/b01/domain/BaseEntity.java
+++ src/main/java/org/zerock/b01/domain/BaseEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "regDate", updatable = false)
+    @Column(name = "regdate", updatable = false)
     private LocalDateTime regDate;
 
     @LastModifiedDate
