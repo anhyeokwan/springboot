f1aa800af899354647fb18e161971f86a4551ca6
anhyeongkwan <bombom841@naver.com>
2023-05-12 00:41:50 +0900

    - 'N+1'과 @BatchSize

--- src/main/java/org/zerock/b01/domain/Board.java
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.domain;
 
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -30,10 +31,11 @@ public class Board extends BaseEntity {
     }
 
     @OneToMany(mappedBy = "board"
-    , cascade = {CascadeType.ALL}
-    ,fetch = FetchType.LAZY
-    , orphanRemoval = true) // BoardImage의 board변수
+            , cascade = {CascadeType.ALL}
+            , fetch = FetchType.LAZY
+            , orphanRemoval = true) // BoardImage의 board변수
     @Builder.Default
+    @BatchSize(size = 20)
     private Set<BoardImage> imageSet = new HashSet<>();
 
     public void addImage(String uuid, String fileName) {
